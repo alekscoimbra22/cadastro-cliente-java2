86d29937d1d754e774c6c879f999f977121126b4
alekscoimbra22 <alekscoimbra@gmail.com>
2025-12-17 11:05:28 -0300

    Endpoint raiz para manter app online

--- src/main/java/novo/project/web/cadastrocliente/CadastroClienteApplication.java
+++ src/main/java/novo/project/web/cadastrocliente/CadastroClienteApplication.java
@@ -2,12 +2,19 @@ package novo.project.web.cadastrocliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CadastroClienteApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CadastroClienteApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String home() {
+        return "<h1>Sistema online com sucesso 🚀</h1>";
+    }
 }
